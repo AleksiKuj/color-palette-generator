40e91fc43683179b287d627a273f5eb5d675e9b3
AleksiKuj <kujala.aleksi99@gmail.com>
2022-12-30 19:37:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const ColorBox = () => {
+  return <div className="color-container">test</div>;
+};
+
+const ColorBoxes = (props) => {
+  return <div className="main-container">{props.boxes}</div>;
+};
 
 function App() {
+  const [boxes, setBoxes] = useState([<ColorBox key={0} />]);
+  const [boxKey, setBoxKey] = useState(1);
+
+  const addBox = () => {
+    if (boxes.length < 10) {
+      setBoxKey(boxKey + 1);
+      setBoxes(boxes.concat(<ColorBox key={boxKey} />));
+    }
+  };
+
+  const removeBox = () => {
+    if (boxes.length > 1) {
+      const boxesCopy = [...boxes];
+      boxesCopy.splice(-1);
+      setBoxes(boxesCopy);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Color templates generator</h1>
+      <button onClick={addBox}>Add </button>
+      <button onClick={removeBox}>Remove</button>
+      <p>Colors: {boxes.length}</p>
+      <ColorBoxes boxes={boxes} />
     </div>
   );
 }
